836b1d75fc17972a73793f765673802a64d1036c
kartarsohal <sohalkartar123@gmail.com>
2022-03-27 18:52:49 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const mainHeader = document.querySelector('.main-header');
 
-hamburgerMenu.setAttribute("src", "../assets/images/icon-hamburger.png");
+hamburgerMenu.setAttribute("src", "./assets/images/icon-hamburger.png");
 
 let menuOpen = false;
 hamburgerMenu.addEventListener("click", (e) => {
   if(!menuOpen) {
-    hamburgerMenu.setAttribute("src", "../assets/images/icon-close.png");
+    hamburgerMenu.setAttribute("src", "./assets/images/icon-close.png");
     mainHeader.classList.add('mobile-header');
     document.body.style.overflow = 'hidden';
     menuOpen = true;
   } else {
-    hamburgerMenu.setAttribute("src", "../assets/images/icon-hamburger.png");
+    hamburgerMenu.setAttribute("src", "./assets/images/icon-hamburger.png");
     mainHeader.classList.remove('mobile-header');
     document.body.style.overflow = 'scroll';
     menuOpen = false;
